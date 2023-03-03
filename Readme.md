f5f1d3daa9fedb0b04f00cac866c11340f2d3b37
rayansami <rayansami007@gmail.com>
2023-03-02 21:29:45 -0500

    send email address & a username to slack channel upon certain POST request

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to create incoming webhook in slack
+
+- Go to https://api.slack.com/ and log in with your Slack account1.
+- Click on “Create an App” and pick a name and a workspace for your app1.
+- After creating, you’ll be redirected to the settings page for your new app. Click on “Incoming Webhooks” under “Features” and toggle the switch to enable them1.
+- Click on “Add New Webhook to Workspace” and choose a channel from the drop-down menu where you want your messages to be posted1.
+- Copy the webhook URL that is generated for you. You will need this URL later when you send messages from PHP1.
+
+- Link: https://honeybadger.notion.site/honeybadger/Take-home-project-for-Software-Developer-position-2023-fee9be3cd8454e1fb61e53f0172ff2e8 
---
+++ slack_notifier.php
@@ -0,0 +1,39 @@
+<?php
+
+//Receive the RAW post data
+$content = trim(file_get_contents("php://input")); // Catch the JSON
+$decoded = json_decode($content);       
+
+// Read the data from JSON Post
+$type = $mysqli->real_escape_string(trim($decoded->{'Type'})); 
+$email = $mysqli->real_escape_string(trim($decoded->{'Email'})); 
+
+// Webhook URL provided for my channel
+$webhook = "https://hooks.slack.com/services/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";
+
+// The message payload to send
+$payload = array(
+    'text' => $email." is shining like a crazy diamond!",
+    'username' => 'WizardDylan'   
+);
+
+if ($type == "SpamNotification"){
+  // Use cURL to send a POST request with JSON data
+    $ch = curl_init($webhook_url); // Init
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+    
+    $result = curl_exec($ch); // execute
+    curl_close($ch); // close curl
+
+    // Check the result
+    if ($result == 'ok') {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: $result";
+    }  
+}
+  
+
+?>
